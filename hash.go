6157b0d0a0247fa00ba2f7bbe145142f0b3563f0
Mikhail Babich <babich-mv@yandex.ru>
2019-05-09 01:38:15 +1000

    Hash: Add sha512

--- hash.go
+++ hash.go
@@ -2,9 +2,14 @@ package utils
 
 import (
 	"crypto/md5"
+	"crypto/sha512"
 	"encoding/base64"
 )
 
 func Md5(key string) string {
 	return base64.URLEncoding.EncodeToString(md5.New().Sum([]byte(key)))
+}
+
+func Sha512(key string) string {
+	return base64.URLEncoding.EncodeToString(sha512.New().Sum([]byte(key)))
 }
